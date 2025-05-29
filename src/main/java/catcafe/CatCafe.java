34cc239c51e5e5b8e03907f37fb84539007695df
ben-274 <ben-274@gmx.de>
2025-05-29 15:55:35 +0200

    CatCafe#getCatByName + CatCafe#getCatByWeight + Main#main mit Optional und Stream implementiert

--- src/main/java/catcafe/CatCafe.java
+++ src/main/java/catcafe/CatCafe.java
@@ -2,6 +2,8 @@ package catcafe;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.Optional;
+
 import tree.Empty;
 import tree.Tree;
 import tree.TreeVisitor;
@@ -34,14 +36,12 @@ public class CatCafe {
      * @param name name of the cat
      * @return cat with the given name
      */
-    public FelineOverLord getCatByName(String name) {
-        if (name == null) return null;
+    public Optional<FelineOverLord> getCatByName(String name) {
+        if (name == null) return Optional.empty();
 
-        for (FelineOverLord c : clowder) {
-            if (c.name().equals(name)) return c;
-        }
-
-        return null;
+        return clowder.stream()
+        		.filter(c -> c.name().equals(name))
+        		.findFirst();
     }
 
     /**
@@ -51,15 +51,12 @@ public class CatCafe {
      * @param maxWeight upper weight limit (exclusive)
      * @return cat within the weight limits
      */
-    public FelineOverLord getCatByWeight(int minWeight, int maxWeight) {
-        if (minWeight < 0) return null;
-        if (maxWeight < minWeight) return null;
+    public Optional<FelineOverLord> getCatByWeight(int minWeight, int maxWeight) {
+        if (minWeight < 0 || maxWeight < minWeight) return Optional.empty();
 
-        for (FelineOverLord c : clowder) {
-            if (c.weight() >= minWeight && c.weight() < maxWeight) return c;
-        }
-
-        return null;
+        return clowder.stream()
+        		.filter(c -> c.weight() >= minWeight && c.weight() < maxWeight)
+        		.findFirst();
     }
 
     /**
